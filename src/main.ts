4e648575db5bbe8508415c66e35e0cb3ab4326bb
Diego-Zarate18 <diegoazarate110800@gmail.com>
2026-03-03 20:39:35 -0600

    Update environment configuration and enable CORS in main application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors( {
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: ['GET'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(`Backend corriendo en: http://localhost:${process.env.PORT}`);
 }
 bootstrap();
